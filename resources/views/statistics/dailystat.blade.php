fe370808271be2e32b95f5628c1d098f77a426b4
Ravager1993 <vincentgandionco@gmail.com>
2020-03-15 15:58:07 +0800

    Periodicstats2
    updated to work with new format

--- resources/views/statistics/dailystat.blade.php
+++ resources/views/statistics/dailystat.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<?php
-    use Carbon\Carbon;
-?>
+
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script> --}}
 {{-- <script src="{{ asset('js/chart.js') }}"></script> --}}
 <script>
